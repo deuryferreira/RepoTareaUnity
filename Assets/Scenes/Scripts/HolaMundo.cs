dc4eeb95460c226296132cce70b028a306fe784e
Deury Ferreira <106875350+deuryferreira@users.noreply.github.com>
2026-09-02 10:15:18 -0400

    Hola Mundo

---
+++ Assets/Scenes/Scripts/HolaMundo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+        print("Esto parece un poquito a python, pero no lo es :(");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
